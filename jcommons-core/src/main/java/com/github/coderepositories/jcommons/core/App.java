4072db11b8e7c938a00aed39d89291d79d214611
zhangguangyong <1243610991@qq.com>
2016-01-17 22:55:57 +0800

    My Java Commons Libs first commit

---
+++ jcommons-core/src/main/java/com/github/coderepositories/jcommons/core/App.java
@@ -0,0 +1,14 @@
+package com.github.coderepositories.jcommons.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		// Yahei Consolas Hybrid
+		// 这个字体，我也是醉了
+		System.out.println("haha");
+	}
+}
