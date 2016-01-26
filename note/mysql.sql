70fd0677ea706a5824fdbfa8a4bbe3401a908600
zhangguangyong <1243610991@qq.com>
2016-01-26 18:16:33 +0800

    commit 2016-01-26 note

---
+++ note/mysql.sql
@@ -0,0 +1,5 @@
+grant all privileges on *.* to 'tmsdb_dev'@'%' identified by "tmsdb_dev" with grant option;
+
+grant all privileges on *.* to 'tmsdev'@'%' identified by "tmsdev" with grant option;
+
+flush privileges;
