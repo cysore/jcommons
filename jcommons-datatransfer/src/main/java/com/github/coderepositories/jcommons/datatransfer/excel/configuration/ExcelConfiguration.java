8af61b92afb6a72c12df5d00bb3dfb53f9b9a7df
zhangguangyong <1243610991@qq.com>
2016-01-28 18:38:43 +0800

    commit

--- jcommons-datatransfer/src/main/java/com/github/coderepositories/jcommons/datatransfer/excel/configuration/ExcelConfiguration.java
+++ jcommons-datatransfer/src/main/java/com/github/coderepositories/jcommons/datatransfer/excel/configuration/ExcelConfiguration.java
@@ -1,6 +1,10 @@
 package com.github.coderepositories.jcommons.datatransfer.excel.configuration;
 
 import java.io.File;
+import java.net.URL;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
 
 public class ExcelConfiguration {
 	/**
@@ -8,6 +12,28 @@ public class ExcelConfiguration {
 	 * @param config
 	 */
 	void loadConfig(File config){
+		XStream xstream = new XStream();
+		xstream.autodetectAnnotations(true);
+		
+		
+		XStreamAlias alias = ExcelLabel.class.getAnnotation(XStreamAlias.class);
+		String rootAlias = alias.value();
+		xstream.alias(rootAlias, ExcelLabel.class);
+		
+		ExcelLabel template = new ExcelLabel();
+		xstream.fromXML(config, template);
+		
+	}
+	
+	
+	void loadConfig(String filepathRelativeToClasspath){
+		
+	}
+	
+	public static void main(String[] args) {
+		URL url = ClassLoader.getSystemResource("");
+		File file = new File(url.getPath(), "data-transfer-excel.xml");
+		System.out.println(file);
 	}
 	
 }
