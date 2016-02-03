3a74cb4bc12867b0c398dc04f46e28d426a8dcb2
zhangguangyong <1243610991@qq.com>
2016-02-03 18:14:11 +0800

    添加 jcommons-plutins-mbg 插件

---
+++ jcommons-plugins/jcommons-plugins-mbg/src/test/java/MyMojoTest.java
@@ -0,0 +1,46 @@
+import com.github.coderepositories.jcommons.plugins.mbg.MyMojo;
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+
+import java.io.File;
+
+/**
+ * Created by Administrator on 2016/2/3.
+ */
+public class MyMojoTest extends AbstractMojoTestCase {
+
+    /**
+     * {@inheritDoc}
+     */
+    protected void setUp()
+            throws Exception {
+        // required
+        super.setUp();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected void tearDown()
+            throws Exception {
+        // required
+        super.tearDown();
+    }
+
+    /**
+     * @throws Exception if any
+     */
+    public void testSomething()
+            throws Exception {
+
+        File pom = getTestFile("src/test/resources/unit/project-to-test/pom.xml");
+        assertNotNull(pom);
+        assertTrue(pom.exists());
+
+        MyMojo myMojo = (MyMojo) lookupMojo("touch", pom);
+        System.out.println(myMojo.getOutputDirectory());
+
+        assertNotNull(myMojo);
+        myMojo.execute();
+    }
+
+}
